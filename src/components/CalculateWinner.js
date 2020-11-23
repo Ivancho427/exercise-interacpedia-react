e0022f1fa3f1561fccba99f87a5284577ffde49b
Ivan Dario Mejia <ivanmm427@gmail.com>
2020-11-23 14:05:03 -0500

    Se corrige cuando hay un ganador ya que no funcionaba bien

--- src/components/CalculateWinner.js
+++ src/components/CalculateWinner.js
@@ -12,7 +12,7 @@ export function calculateWinner(squares) {
 
   for (let i = 0; i < winningLines.length; i++) {
     const [a, b, c] = winningLines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[c]) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
